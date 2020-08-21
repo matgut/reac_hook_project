acc625baa69261737e854f94a4f901e9dd742e5c
cgutierm <c.gutierrez2511@gmail.com>
2020-08-21 10:44:43 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,87 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState, useCallback, useEffect} from 'react';
 import './App.css';
 
-function App() {
+const App = () =>{
+  
+  const [newTask,setNewTask] = useState('');//useState devuelve un array name = variable, setName = funcion para cambios
+  const [task,setTask] = useState([]);
+
+  const onNewTaskChange = useCallback(
+    (e) => {
+      setNewTask(e.target.value);
+    },
+    [],
+  )
+
+  const formSubmitted = useCallback(
+    (e) => {
+      e.preventDefault();
+      if (!newTask.trim()) return;
+      setTask([
+        ...task,
+        {
+          id: task.length + 1,
+          content: newTask,
+          done: false
+        }
+      ]);
+      setNewTask('');
+    },
+    [newTask,task],
+  )
+
+  useEffect(() =>{
+    console.log(task)
+  },
+  [task]
+  );
+
+  const onChangeCheck = useCallback(
+      (tasks,index) => (e) => {
+        const newTasks = [...task];
+        newTasks.splice(index, 1, {
+          ...tasks,
+          done: !tasks.done
+        });
+        setTask(newTasks);
+      },
+    [task],
+  );
+
+  const removeTask = useCallback(
+      (tasks) => (e) =>{
+        setTask(task.filter(otherTask => otherTask !== tasks))
+      },
+    [task],
+  )
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+        <form onSubmit={formSubmitted}>
+          <label> enter your task:</label>
+          <input
+            type="text"
+            onChange={onNewTaskChange}
+            name="newTask"
+            value={newTask}
+          />
+          <button>add task</button>
+        </form>
+        <ul>
+          {
+            task.map((tk, index) => (
+              <li key={tk.id}>
+                <input 
+                  type="checkbox"
+                  checked={task.done}
+                  onChange={onChangeCheck(tk, index)}
+                />
+                <span className={tk.done ? 'done' : ''}>{tk.content}</span>
+                <button onClick={removeTask(tk)}>remove task</button>
+              </li>
+            ))
+          }
+        </ul>
     </div>
   );
 }
